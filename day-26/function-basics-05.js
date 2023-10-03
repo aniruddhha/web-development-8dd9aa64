7bc6c8350b0b2df9f149d9e3e1aa90ff16c590a6
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-10-03 11:54:36 +0530

    es6 : arrow functions

---
+++ day-26/function-basics-05.js
@@ -0,0 +1,66 @@
+function speedUp1(spd) {
+    // const speed = spd * 2
+    // return speed
+
+    return spd * 2
+}
+
+speedUp1(10)
+
+const speedUp2 = function(spd) {
+    // const speed = spd * 2
+    // return speed
+
+    return spd * 2
+}
+
+speedUp2(10)
+
+const speedUp3 = spd => {
+    // const speed = spd * 2
+    // return speed
+
+    return spd * 2
+}
+
+speedUp3(10)
+
+const speedUp4 = mxzcvn => mxzcvn * 2
+
+speedUp4(10)
+
+function changeColor1(r, g, b) {
+    let red = r, green = g, blue = b;
+    if(!r)
+        red = Math.random() * 256 // 0.2 * 256
+    if(!g)
+        green = Math.random() * 256
+    if(!b) 
+        blue = Math.random() * 256
+
+    return `${red}, ${green}, ${blue}`
+}
+
+const changeColor2 = (r, g, b) => {
+    
+    let red = r , green = g , blue = b ;
+    if(!r)
+        red = Math.random() * 256 // 0.2 * 256
+    if(!g)
+        green = Math.random() * 256
+    if(!b) 
+        blue = Math.random() * 256
+    return `${red}, ${green}, ${blue}`
+}
+
+const changeColor3 = (r, b, g) => {
+    let red = r ? r : Math.random() * 256 , green = g ? g : Math.random() * 256, blue = b ? b : Math.random() * 256;
+    return `${red}, ${green}, ${blue}`
+}
+
+const changeColor4 = (r, b, g) => {
+    return `${r ? r : Math.random() * 256}, ${g ? g : Math.random() * 256}, ${b ? b : Math.random() * 256}`
+}
+
+const changeColor5 = (r, b, g) => `${r ? r : Math.random() * 256}, ${g ? g : Math.random() * 256}, ${b ? b : Math.random() * 256}`
+
