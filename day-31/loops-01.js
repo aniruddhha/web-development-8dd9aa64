d20315fc6a3b613e0cdd31ad1a0997ccb55985f4
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-10-16 11:31:05 +0530

    loops : basics of loops

---
+++ day-31/loops-01.js
@@ -0,0 +1,27 @@
+
+function simpleFor() {
+    console.log(`🟢 For Loop 🟢`)
+    for (let i = 0; i < 10; i++) {
+        console.log(`Hi`)
+    }
+}
+
+function whileLoop() {
+    console.log(`🟢 While Loop 🟢`)
+    let i = 0
+    while ( i < 10 ) {
+        console.log(`Hi`)
+        i++
+    }
+}
+
+function doWhile() {
+    console.log(`🟢 Do While Loop 🟢`)
+    let i = 0;
+    do {
+        console.log(`Hi`)
+        i++
+    } while( i < 10 )
+}
+
+doWhile()
